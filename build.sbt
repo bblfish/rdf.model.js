ffc3dc7fb4f26434fb9fa7128fc3c516683bf131
Henry Story <henry.story@bblfish.net>
2021-11-24 10:22:32 +0100

    reformat code.

--- build.sbt
+++ build.sbt
@@ -5,34 +5,11 @@ import sbt.url
 
 scalaVersion := Ver.scala3
 
-ThisBuild / homepage      := Some(url("https://github.com/bblfish/rdf.scala.js"))
-ThisBuild / licenses      += ("Apache 2.0", url("https://opensource.org/licenses/Apache-2.0"))
-ThisBuild / organization  := "net.bblfish.rdf"
-ThisBuild / shellPrompt   := ((s: State) => Project.extract(s).currentRef.project + "> ")
+ThisBuild / homepage := Some(url("https://github.com/bblfish/rdf.scala.js"))
+ThisBuild / licenses += ("Apache 2.0", url("https://opensource.org/licenses/Apache-2.0"))
+ThisBuild / organization := "net.bblfish.rdf"
+ThisBuild / shellPrompt := ((s: State) => Project.extract(s).currentRef.project + "> ")
 ThisBuild / versionScheme := Some("early-semver")
-
-val scala3jsOptions =  Seq(
-	// "-classpath", "foo:bar:...",         // Add to the classpath.
-	//"-encoding", "utf-8",                // Specify character encoding used by source files.
-	"-deprecation",                      // Emit warning and location for usages of deprecated APIs.
-	"-unchecked",                        // Enable additional warnings where generated code depends on assumptions.
-	"-feature",                          // Emit warning and location for usages of features that should be imported explicitly.
-	//"-explain",                          // Explain errors in more detail.
-	//"-explain-types",                    // Explain type errors in more detail.
-	"-indent",                           // Together with -rewrite, remove {...} syntax when possible due to significant indentation.
-	// "-no-indent",                        // Require classical {...} syntax, indentation is not significant.
-	"-new-syntax",                       // Require `then` and `do` in control expressions.
-	// "-old-syntax",                       // Require `(...)` around conditions.
-	// "-language:Scala2",                  // Compile Scala 2 code, highlight what needs updating
-	//"-language:strictEquality",          // Require +derives Eql+ for using == or != comparisons
-	// "-rewrite",                          // Attempt to fix code automatically. Use with -indent and ...-migration.
-	// "-scalajs",                          // Compile in Scala.js mode (requires scalajs-library.jar on the classpath).
-	"-source:future",                       // Choices: future and future-migration. I use this to force future deprecation warnings, etc.
-	// "-Xfatal-warnings",                  // Fail on warnings, not just errors
-	// "-Xmigration",                       // Warn about constructs whose behavior may have changed since version.
-	// "-Ysafe-init",                       // Warn on field access before initialization
-	"-Yexplicit-nulls"                  // For explicit nulls behavior.
-)
 
 lazy val commonSettings = Seq(
 	name := "rdf-model-js",
@@ -42,29 +19,26 @@ lazy val commonSettings = Seq(
 	scalaVersion := Ver.scala3,
 	updateOptions := updateOptions.value.withCachedResolution(true) //to speed up dependency resolution
 )
-
 lazy val publicationSettings = Seq(
-			sonatypeProfileName := "net.bblfish.rdf",
-			publishTo := sonatypePublishToBundle.value,
+	sonatypeProfileName := "net.bblfish.rdf",
+	publishTo := sonatypePublishToBundle.value,
 
-			// To sync with Maven central, you need to supply the following information:
-			publishMavenStyle := true,
+	// To sync with Maven central, you need to supply the following information:
+	publishMavenStyle := true,
 
-			// Open-source license of your choice
-			licenses +=("Apache 2.0", url("https://opensource.org/licenses/Apache-2.0")),
-			homepage := Some(url("https://github.com/banana-rdf/")),
-			scmInfo := Some(
-				ScmInfo(
-					url("https://github.com/bblfish/rdf.scala.js"),
-					"git@github.com:bblfish/rdf.scala.js.git"
-				)
-			),
-			developers := List(
-				Developer(id="bblfish", name="Henry Story", email="henry.story@bblfish.net", url=url("https://bblfish.net/")),
-			)
+	// Open-source license of your choice
+	licenses += ("Apache 2.0", url("https://opensource.org/licenses/Apache-2.0")),
+	homepage := Some(url("https://github.com/banana-rdf/")),
+	scmInfo := Some(
+		ScmInfo(
+			url("https://github.com/bblfish/rdf.scala.js"),
+			"git@github.com:bblfish/rdf.scala.js.git"
 		)
-
-
+	),
+	developers := List(
+		Developer(id = "bblfish", name = "Henry Story", email = "henry.story@bblfish.net", url = url("https://bblfish.net/")),
+	)
+)
 lazy val rdfModelJS = project.in(file("."))
 	.enablePlugins(ScalaJSPlugin)
 	//documentation here: https://scalablytyped.org/docs/library-developer
@@ -80,8 +54,30 @@ lazy val rdfModelJS = project.in(file("."))
 		//see module doc https://www.scala-js.org/doc/project/module.html
 		scalaJSLinkerConfig ~= {
 			_.withModuleKind(ModuleKind.ESModule)
-//				 nodjs needs .mjs extension. See https://www.scala-js.org/doc/project/module.html
+				//				 nodjs needs .mjs extension. See https://www.scala-js.org/doc/project/module.html
 				.withOutputPatterns(OutputPatterns.fromJSFile("%s.mjs"))
 		}
 		// scalaJSLinkerConfig ~= { _.withModuleKind(ModuleKind.CommonJSModule) }
 	)
+val scala3jsOptions = Seq(
+	// "-classpath", "foo:bar:...",         // Add to the classpath.
+	//"-encoding", "utf-8",                // Specify character encoding used by source files.
+	"-deprecation", // Emit warning and location for usages of deprecated APIs.
+	"-unchecked", // Enable additional warnings where generated code depends on assumptions.
+	"-feature", // Emit warning and location for usages of features that should be imported explicitly.
+	//"-explain",                          // Explain errors in more detail.
+	//"-explain-types",                    // Explain type errors in more detail.
+	"-indent", // Together with -rewrite, remove {...} syntax when possible due to significant indentation.
+	// "-no-indent",                        // Require classical {...} syntax, indentation is not significant.
+	"-new-syntax", // Require `then` and `do` in control expressions.
+	// "-old-syntax",                       // Require `(...)` around conditions.
+	// "-language:Scala2",                  // Compile Scala 2 code, highlight what needs updating
+	//"-language:strictEquality",          // Require +derives Eql+ for using == or != comparisons
+	// "-rewrite",                          // Attempt to fix code automatically. Use with -indent and ...-migration.
+	// "-scalajs",                          // Compile in Scala.js mode (requires scalajs-library.jar on the classpath).
+	"-source:future", // Choices: future and future-migration. I use this to force future deprecation warnings, etc.
+	// "-Xfatal-warnings",                  // Fail on warnings, not just errors
+	// "-Xmigration",                       // Warn about constructs whose behavior may have changed since version.
+	// "-Ysafe-init",                       // Warn on field access before initialization
+	"-Yexplicit-nulls" // For explicit nulls behavior.
+)
--- src/main/scala/run/cosy/rdfjs/model/Model.scala
+++ src/main/scala/run/cosy/rdfjs/model/Model.scala
@@ -9,50 +9,53 @@ import scala.scalajs.js.annotation.{JSExport, JSExportAll, JSExportTopLevel}
  * with a few (documented) extra tweaks for compatibility with rdflib.js
  */
 trait Term[Type <: Term.TT](@JSExport val termType: Type):
-  @JSExport("equals")
-  def termEquals(other: js.UndefOr[Term[?]]): Boolean =
-    other.map(o => this.equals(o)).getOrElse(false)
+	@JSExport("equals")
+	def termEquals(other: js.UndefOr[Term[?]]): Boolean =
+		other.map(o => this.equals(o)).getOrElse(false)
 end Term
 
-trait ValueTerm[Type <: Term.TT] extends Term[Type]:
-  val value: String
+trait ValueTerm[Type <: Term.TT] extends Term[Type] :
+	val value: String
 
 @JSExportAll
 object Term:
-  type TT =  VTT | Quad.type
-  type VTT = NamedNode.type | Literal.type | Variable.type | BlankNode.type | DefaultGraph.type
+	type TT = VTT | Quad.type
+	type VTT = NamedNode.type | Literal.type | Variable.type | BlankNode.type | DefaultGraph.type
 
-  val NamedNode: "NamedNode" =  "NamedNode"
-  val Literal: "Literal" = "Literal"
-  val Variable: "Variable" = "Variable"
-  val BlankNode: "BlankNode" = "BlankNode"
-  val DefaultGraph: "DefaultGraph" = "DefaultGraph"
-  val Quad: "Quad" = "Quad"
+	val NamedNode: "NamedNode" = "NamedNode"
+	val Literal: "Literal" = "Literal"
+	val Variable: "Variable" = "Variable"
+	val BlankNode: "BlankNode" = "BlankNode"
+	val DefaultGraph: "DefaultGraph" = "DefaultGraph"
+	val Quad: "Quad" = "Quad"
 end Term
 
 @JSExportTopLevel("NamedNode")
 @JSExportAll
 case class NamedNode(override val value: String) extends
-  ValueTerm[Term.NamedNode.type], Term(Term.NamedNode)
+	ValueTerm[Term.NamedNode.type], Term(Term.NamedNode)
 
 @JSExportAll
-case class Literal private (
-  override val value: String, val language: String, val datatype: NamedNode
-) extends ValueTerm[Term.Literal.type], Term(Term.Literal):
+case class Literal private(
+	override val value: String, val language: String, val datatype: NamedNode
+) extends ValueTerm[Term.Literal.type], Term(Term.Literal) :
 end Literal
 
 object Literal:
-  lazy val rdfLangStr = NamedNode( "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")
-  lazy val xsdStr = NamedNode("http://www.w3.org/2001/XMLSchema#string")
-  def apply(value: String): Literal = new Literal(value,"",xsdStr)
-  def apply(value: String, lang: String) = new Literal(value,lang,rdfLangStr)
-  def apply(value: String, dataType: NamedNode) = new Literal(value,"",dataType)
+	lazy val rdfLangStr = NamedNode("http://www.w3.org/1999/02/22-rdf-syntax-ns#langString")
+	lazy val xsdStr = NamedNode("http://www.w3.org/2001/XMLSchema#string")
+
+	def apply(value: String): Literal = new Literal(value, "", xsdStr)
+
+	def apply(value: String, lang: String) = new Literal(value, lang, rdfLangStr)
+
+	def apply(value: String, dataType: NamedNode) = new Literal(value, "", dataType)
 end Literal
 
 //blank nodes should be created via the factory
 @JSExportAll
-case class BlankNode private[rdfjs] (val value: String)
-  extends ValueTerm[Term.BlankNode.type], Term(Term.BlankNode)
+case class BlankNode private[rdfjs](val value: String)
+	extends ValueTerm[Term.BlankNode.type], Term(Term.BlankNode)
 
 @JSExportTopLevel("Variable")
 @JSExportAll
@@ -64,81 +67,90 @@ case class Variable(val value: String) extends ValueTerm[Term.Variable.type], Te
 @JSExportTopLevel("DefaultGraph")
 @JSExportAll
 case class DefaultGraph(val value: String = "chrome:theSession")
-  extends ValueTerm[Term.DefaultGraph.type], Term(Term.DefaultGraph)
+	extends ValueTerm[Term.DefaultGraph.type], Term(Term.DefaultGraph)
 
-case class Quad private[rdfjs] (
-  @JSExport("subject") val subj: Quad.Subject,
-  @JSExport("predicate") val rel: Quad.Predicate,
-  @JSExport("object") val obj: Quad.Object,
-  @JSExport("graph") val graph: Quad.Graph
+case class Quad private[rdfjs](
+	@JSExport("subject") val subj: Quad.Subject,
+	@JSExport("predicate") val rel: Quad.Predicate,
+	@JSExport("object") val obj: Quad.Object,
+	@JSExport("graph") val graph: Quad.Graph
 ) extends Term(Term.Quad)
 
 object Quad:
-  type Subject = BlankNode | NamedNode
-  type Predicate = NamedNode
-  type Object = BlankNode | NamedNode | Literal
-  type Graph = BlankNode | NamedNode | DefaultGraph
+	type Subject = BlankNode | NamedNode
+	type Predicate = NamedNode
+	type Object = BlankNode | NamedNode | Literal
+	type Graph = BlankNode | NamedNode | DefaultGraph
 
 @JSExportAll
 class DataFactory(bnodeStart: BigInt = DataFactory.one):
 
-  private val bnodeCounter: Iterator[BigInt] = new Iterator[BigInt] {
-    var nextNode = bnodeStart
-    override def hasNext: Boolean = true
-    override def next(): BigInt =
-      nextNode = nextNode + DataFactory.one
-      nextNode
-  }
-  private val bnode: Iterator[String] = bnodeCounter.map(_.toString(Character.MAX_RADIX))
+	private val bnodeCounter: Iterator[BigInt] = new Iterator[BigInt] {
+		var nextNode = bnodeStart
 
-  def namedNode(value: String): NamedNode = NamedNode(value)
+		override def hasNext: Boolean = true
 
-  def literal(value: String): Literal = Literal(value)
-  def literal(value: String, lang: String): Literal = Literal(value,lang)
-  def literal(value: String, dataType: NamedNode): Literal = Literal(value,dataType)
+		override def next(): BigInt =
+			nextNode = nextNode + DataFactory.one
+			nextNode
+	}
+	private val bnode: Iterator[String] = bnodeCounter.map(_.toString(Character.MAX_RADIX))
 
-  def variable(value: String): Variable = Variable(value)
+	def namedNode(value: String): NamedNode = NamedNode(value)
 
-  def blankNode(): BlankNode =
-    BlankNode(bnode.next())
-  def blankNode(label: String): BlankNode =
-    if !label.contains('#') then BlankNode(label)
-    else BlankNode(bnode.next())
+	def literal(value: String): Literal = Literal(value)
 
-  def quad(
-    subject: Quad.Subject, rel: Quad.Predicate, obj: Quad.Object
-  ): Quad = Quad(subject, rel, obj, defaultGraph)
-  def quad(
-    subject: Quad.Subject, rel: Quad.Predicate, obj: Quad.Object,
-    graph: Quad.Graph
-  ): Quad = Quad(subject, rel, obj, graph)
+	def literal(value: String, lang: String): Literal = Literal(value, lang)
 
-  val defaultGraph: DefaultGraph = new DefaultGraph()
+	def literal(value: String, dataType: NamedNode): Literal = Literal(value, dataType)
 
-  type Nodes = BlankNode | NamedNode | Literal | DefaultGraph | Variable
-  /**
-   * Required by rdflib.js which states:
-   * Generates a uniquely identifiably *idempotent* string for the given {term}.
-   * Equivalent to "Term.hashString"x§
-   * @example Use this to associate data with a term in an object { obj[id(term)] = "myData" }
-   */
-  def id(term: Nodes): String =
-    term.toString
+	def variable(value: String): Variable = Variable(value)
+
+	def blankNode(): BlankNode =
+		BlankNode(bnode.next())
+
+	def blankNode(label: String): BlankNode =
+		if !label.contains('#') then BlankNode(label)
+		else BlankNode(bnode.next())
+
+	def quad(
+		subject: Quad.Subject, rel: Quad.Predicate, obj: Quad.Object
+	): Quad = Quad(subject, rel, obj, defaultGraph)
+
+	def quad(
+		subject: Quad.Subject, rel: Quad.Predicate, obj: Quad.Object,
+		graph: Quad.Graph
+	): Quad = Quad(subject, rel, obj, graph)
+
+	val defaultGraph: DefaultGraph = new DefaultGraph()
+
+	type Nodes = BlankNode | NamedNode | Literal | DefaultGraph | Variable
+
+	/**
+	 * Required by rdflib.js which states:
+	 * Generates a uniquely identifiably *idempotent* string for the given {term}.
+	 * Equivalent to "Term.hashString"x§
+	 *
+	 * @example Use this to associate data with a term in an object { obj[id(term)] = "myData" }
+	 */
+	def id(term: Nodes): String =
+		term.toString
 
 end DataFactory
 
 //todo: how do frameworks make sure blank nodes don't end up clashing in their store?
 @JSExportTopLevel("DataFactory")
 object DataFactory:
-  val one: BigInt = BigInt(1)
-  @JSExport("create")
-  def apply(): DataFactory = new DataFactory(one)
+	val one: BigInt = BigInt(1)
+
+	@JSExport("create")
+	def apply(): DataFactory = new DataFactory(one)
 
 object Test:
-  def main(args: Array[String]): Unit =
-    val bbl = new NamedNode("https://bblfish.net/#me")
-    val bbl2 = new NamedNode("https://bblfish.net/#me")
-    println(s"$bbl == bbl2 is "+ (bbl == bbl2))
+	def main(args: Array[String]): Unit =
+		val bbl = new NamedNode("https://bblfish.net/#me")
+		val bbl2 = new NamedNode("https://bblfish.net/#me")
+		println(s"$bbl == bbl2 is " + (bbl == bbl2))
 
 
 
