43e843650c7acf67717878c8b9e358392fe84a05
Henry Story <henry.story@bblfish.net>
2022-02-16 17:40:04 +0100

    use sbt-typelevel and setup gihub flow

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,23 +1,24 @@
+//selenium testing
+libraryDependencies += "org.scala-js" %% "scalajs-env-selenium" % "1.1.1"
+
+//for sbt-typelevel when using SNAPSHOTS
+resolvers += Resolver.sonatypeRepo("snapshots")
 
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.7.1")
 
-/**
- * allows one to access JS npm distributions with the ease with which one can work with maven
- * @see https://search.maven.org/search?q=a:sbt-scalajs-bundler
- * @see https://scalacenter.github.io/scalajs-bundler/getting-started.html
- */
+/** allows one to access JS npm distributions with the ease with which one can work with maven
+  * @see
+  *   https://search.maven.org/search?q=a:sbt-scalajs-bundler
+  * @see
+  *   https://scalacenter.github.io/scalajs-bundler/getting-started.html
+  * @see
+  *   https://github.com/scalacenter/scalajs-bundler
+  * @see
+  *   https://scalacenter.github.io/scalajs-bundler/
+  */
 addSbtPlugin("ch.epfl.scala" % "sbt-scalajs-bundler" % "0.21.0-RC1")
 
-/**
-* sbtsonatype plugin used to publish artifact to maven central via sonatype nexus
-* @see https://github.com/xerial/sbt-sonatype
-*/
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "3.9.10")
-
-
-/**
- * plugin used to sign the artifcat with pgp keys
- * @see https://github.com/sbt/sbt-pgp
- */
-addSbtPlugin("com.github.sbt" % "sbt-pgp" % "2.1.2")
-
+// https://typelevel.org/sbt-typelevel/index.html
+addSbtPlugin("org.typelevel" % "sbt-typelevel" % "0.4.3")
+addSbtPlugin("org.scala-js"  % "sbt-scalajs"   % "1.8.0")
+addSbtPlugin("com.eed3si9n"  % "sbt-buildinfo" % "0.10.0")
--- src/test/scala/run/cosy/rdfjs/model/TestModel.scala
+++ src/test/scala/run/cosy/rdfjs/model/TestModel.scala
@@ -1,40 +1,45 @@
+/*
+ * Copyright 2021 Henry Story
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package run.cosy.rdfjs.model
 
 import utest.*
 
-object TestModel extends TestSuite {
+object TestModel extends TestSuite:
 
-	val bblStr = "https://bblfish.net/#me"
-	val df = DataFactory()
+   val bblStr = "https://bblfish.net/#me"
+   val df     = DataFactory()
 
-	def foaf(att: String)(using df: DataFactory): NamedNode =
-		df.namedNode("https://xmlns.com/foaf/0.1/" + att)
+   def foaf(att: String)(using df: DataFactory): NamedNode =
+     df.namedNode("https://xmlns.com/foaf/0.1/" + att)
 
-	def tests = Tests {
-		test("the same Named Nodes is  Equal to itself") {
-			val bbl = NamedNode(bblStr)
-			val bbl2 = NamedNode(bblStr)
-			assert(bbl == bbl2)
-		}
+   def tests = Tests {
+     test("the same Named Nodes is  Equal to itself") {
+       val bbl  = NamedNode(bblStr)
+       val bbl2 = NamedNode(bblStr)
+       assert(bbl == bbl2)
+     }
 
-		test("the factory") {
-			val bbl1 = df.namedNode(bblStr)
-			val bbl2 = NamedNode(bblStr)
-			assert(bbl1 == bbl2)
-			val fname = df.namedNode("name")
-			val bblName = df.literal("Henry")
-			val st = df.quad(bbl1, fname, bblName)
-			assert(st.subj == bbl2)
-			assert(st.obj == bblName)
-			assert(st.rel == fname)
-			assert(st.graph == df.defaultGraph)
-		}
+     test("the factory") {
+       val bbl1 = df.namedNode(bblStr)
+       val bbl2 = NamedNode(bblStr)
+       assert(bbl1 == bbl2)
+       val fname   = df.namedNode("name")
+       val bblName = df.literal("Henry")
+       val st      = df.quad(bbl1, fname, bblName)
+       assert(st.subj == bbl2)
+       assert(st.obj == bblName)
+       assert(st.rel == fname)
+       assert(st.graph == df.defaultGraph)
+     }
 
-		test("Factory.id") {
-			println(df.namedNode(bblStr))
-			println(df.literal("Hello"))
-			println(df.defaultGraph)
-			println(df.blankNode("l1"))
-		}
-	}
-}
+     test("Factory.id") {
+       println(df.namedNode(bblStr))
+       println(df.literal("Hello"))
+       println(df.defaultGraph)
+       println(df.blankNode("l1"))
+     }
+   }
